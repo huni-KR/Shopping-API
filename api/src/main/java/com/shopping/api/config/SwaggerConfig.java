5f179fbb6c5d3e67e4656b59fa66af4e990eee2b
huni <xogns12356@gmail.com>
2022-06-13 03:06:37 +0900

    Build : swagger 설정 변경

--- api/src/main/java/com/shopping/api/config/SwaggerConfig.java
+++ api/src/main/java/com/shopping/api/config/SwaggerConfig.java
@@ -1,25 +1,24 @@
 package com.shopping.api.config;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //http://localhost:8080/swagger-ui/index.html
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
-	@Bean
-	public Docket postsApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("shoppingAPI").select()
-				.apis(RequestHandlerSelectors.basePackage("com.shopping.api.controller")).paths(regex("/api/.*"))
-				.build();
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
